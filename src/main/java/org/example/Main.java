06c56975579a66c598ceb3be7291b20e48a6b7c2
JSL <slinshenchu@163.com>
2024-09-16 16:15:18 +0800

    master 第4次提交

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,6 @@ public class Main {
 
         // 按 Shift+F10 或点击间距中的绿色箭头按钮以运行脚本。
         for (int i = 1; i <= 8; i++) {
-
             // 按 Shift+F9 开始调试代码。我们已为您设置了一个断点，
             // 但您始终可以通过按 Ctrl+F8 添加更多断点。
             System.out.println("i = " + "蒋松林");
